f09744e11f4e5badda3d4c7079c5e721ad8d7052
shadid12 <shadidhaque2014@gmail.com>
2018-03-04 18:39:19 -0500

    First Commit now

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+
+
+const productRoutes = require('./api/routes/products');
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// CORS
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+
+app.use('/products', productRoutes);
+
+module.exports = app;
